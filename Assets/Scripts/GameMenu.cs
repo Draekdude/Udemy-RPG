167b55cb5e48c0af421e8153fdf7044de8df2123
jboardman <Julian.Boardman@pacelabs.com>
2021-03-27 22:04:08 -0500

    53 - working on Item Window

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -17,6 +17,11 @@ public class GameMenu : MonoBehaviour
     public GameObject[] charStatHolder;
     public List<GameObject> windows;
 
+    public List<GameObject> statusButtons;
+
+    public GameObject playerStatLabels;
+    public Image statusImage;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -63,8 +68,26 @@ public class GameMenu : MonoBehaviour
         }
     }
 
+    public void UpdatePlayerStats(int playerIndex)
+    {
+        playerStats = GameManager.instance.playersStats;
+        var textValueFields = playerStatLabels.GetComponentsInChildren<Text>();
+        textValueFields.SingleOrDefault(item => item.name == "Name").text = playerStats[playerIndex].charName;
+        textValueFields.SingleOrDefault(item => item.name == "Name").text = playerStats[playerIndex].charName;
+        textValueFields.SingleOrDefault(item => item.name == "HP").text = $"{playerStats[playerIndex].currentHp}/{playerStats[playerIndex].maxHp}";
+        textValueFields.SingleOrDefault(item => item.name == "MP").text = $"{playerStats[playerIndex].currentMp}/{playerStats[playerIndex].maxMp}";
+        textValueFields.SingleOrDefault(item => item.name == "Strength").text = playerStats[playerIndex].strength.ToString();
+        textValueFields.SingleOrDefault(item => item.name == "Defence").text = playerStats[playerIndex].defense.ToString();
+        textValueFields.SingleOrDefault(item => item.name == "EquippedWeapon").text = playerStats[playerIndex].equippedWeapon == null ? playerStats[playerIndex].equippedWeapon : "None";
+        textValueFields.SingleOrDefault(item => item.name == "EquippedArmor").text = playerStats[playerIndex].equippedArmor == null ? playerStats[playerIndex].equippedArmor : "None";
+        textValueFields.SingleOrDefault(item => item.name == "ArmorPower").text = playerStats[playerIndex].armorPower.ToString();
+        textValueFields.SingleOrDefault(item => item.name == "XpToNextLevel").text = $"{playerStats[playerIndex].expToNextLevel[playerStats[playerIndex].playerLevel] - playerStats[playerIndex].currentExp}";
+        statusImage.sprite = playerStats[playerIndex].characterImage;
+    }
+
     public void ToggleWindow(int windowIndex)
     {
+        UpdateMainStats();
         bool isActive = windows[windowIndex].activeInHierarchy;
         windows.ForEach(w => w.SetActive(false));
         windows[windowIndex].SetActive(!isActive);
@@ -75,5 +98,17 @@ public class GameMenu : MonoBehaviour
         windows.ForEach(w => w.SetActive(false));
         menu.SetActive(false);
         GameManager.instance.gameMenuOpen = false;
+    }
+
+    public void OpenStatus()
+    {
+        UpdateMainStats();
+        for (int i = 0; i < statusButtons.Count; i++)
+        {
+            statusButtons[i].SetActive(playerStats[i].gameObject.activeInHierarchy);
+            statusButtons[i].GetComponentInChildren<Text>().text = playerStats[i].charName;
+            
+        }
+        UpdatePlayerStats(0);
     }
 }
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [Header("Item Type")]
+    public bool isItem;
+    public bool isWeapon;
+    public bool isArmor;
+
+    [Header("Item Details")]
+    public string itemName;
+    public string description;
+    public int value;
+    public Sprite image;
+
+    [Header("Item Effects")]
+    public int amountToChange;
+    public bool affectHp, affectMp, affectStrength;
+    public int weaponStrength;
+    public int armorStrength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
